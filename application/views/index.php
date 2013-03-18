4a7e5e991fa966b19dee2ab4aba59ab9ae5bc4f9
dima11221122 <dimansny@bk.ru>
2013-03-19 00:06:19 +0400

    Доделал слайдер

--- application/views/index.php
+++ application/views/index.php
@@ -66,6 +66,30 @@
                 </tr>
             </table>
         </section>
+        <footer>
+            <div class="centerSpan">
+                <div class="share42init"></div>
+                <script type="text/javascript" src="/js/share42/share42.js"></script>
+                <script type="text/javascript">share42('http://fitpark.com/js/share42/')</script>
+            </div>
+            <div class="centerSpan">
+                <p>
+                    Whidofit. On-line дневник для контроля за питанием, физическими нагрузками и образом жизни.
+                    <br>Удобный поиск по упражнениям для фитнеса и бодибилдинга, автоматический калькулятор
+                    калорий и питательных веществ, услуги диетологов и фитнес-тренеров.
+                </p>
+                <hr/>
+                <p>Whidofit. Все права защищены. 2012</p>
+                <p>
+                    <a class="link" href="https://passport.webmoney.ru/asp/certview.asp?wmid=130348514579" target="_blank" style="display: inline; margin-right: 10px;">
+                        <img src="http://whido.ru/img/88x31_wm_v_blue_on_transparent_ru.png" title="Здесь находится аттестат нашего WM идентификатора 130348514579" style="border:none;" alt="">
+                    </a>
+                    <a class="link" href="http://webmoney.ru" target="_blank" style="display: inline;">
+                        <img src="http://whido.ru/img/88x31_wm_blue_on_transparent_ru.png" style="border:none;" alt="">
+                    </a>
+                </p>
+            </div>
+        </footer><!--footer-->
     </body>
 </html>
 
