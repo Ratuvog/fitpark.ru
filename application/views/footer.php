9173df48939994cb04da4679af62207e3177300a
dima11221122 <dimansny@bk.ru>
2013-05-02 22:29:00 +0400

    #7. Подвал должен быть на каждой странице

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,18 @@
         </section>
-
+<footer>
+            <div class="centerSpan">
+                <div class="share42init"></div>
+                <script type="text/javascript" src="/js/share42/share42.js"></script>
+                <script type="text/javascript">share42('http://fitpark.com/js/share42/')</script>
+            </div>
+            <div class="centerSpan">
+                <p>Проводник в мир фитнеса «ФитПарк». Фитнес клубы, расписание тренировок, цены абонементов, отзывы.</p>
+                <p>Самара фитнес.</p>
+                <p>
+                   «ФитПарк» 2012-2013. Все права защищены.
+                </p>
+                <hr/>
+            </div>
+        </footer>
     </body>
 </html>
