3d8225fc9559bc5617293c0278c25eb6b14bc914
dima11221122 <dimansny@bk.ru>
2013-06-16 00:47:47 +0400

    #125. Добавил виджет для оставления отзывов через сервис http://reformal.ru/

--- application/views/footer.php
+++ application/views/footer.php
@@ -77,6 +77,24 @@
 <!--<img src="//top-fwz1.mail.ru/counter?id=2360902;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />-->
 <!--</div></noscript>-->
 <!--<!-- //Rating@Mail.ru counter -->-->
+<script type="text/javascript">
+    var reformalOptions = {
+        project_id: 105039,
+        project_host: "fitpark.reformal.ru",
+        tab_orientation: "left",
+        tab_indent: "50%",
+        tab_bg_color: "#03b003",
+        tab_border_color: "#FFFFFF",
+        tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/2a94cfe6511106e7a48d0af3904e3090/left/1/tab.png",
+        tab_border_width: 2
+    };
 
+    (function() {
+        var script = document.createElement('script');
+        script.type = 'text/javascript'; script.async = true;
+        script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
+</script><noscript><a href="http://reformal.ru"><img src="http://media.reformal.ru/reformal.png" /></a><a href="http://fitpark.reformal.ru">Oтзывы и предложения для Фитпарк</a></noscript>
     </body>
 </html>
