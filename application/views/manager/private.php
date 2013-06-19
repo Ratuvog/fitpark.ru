eb8b2abef93cf88465eb82a24dbbb53be73e7f75
ratuvog <valkiriya.nd@gmail.com>
2013-06-19 20:32:15 +0400

    Недолил вьюху

--- application/views/manager/private.php
+++ application/views/manager/private.php
@@ -16,50 +16,63 @@
 
 <div class="info-block">
     <div class="title">Общая информация</div>
-    <div class="box">
+    <div class="save-form box">
         <div class="user-input">
             <label class="caption">Наименование</label>
-            <input type="text" placeholder="Наименование"/>
+            <input name="name" type="text" placeholder="Наименование" value="<?=$club->name;?>" validator="blank"/>
         </div>
         <div class="user-input">
             <label class="caption">Сайт</label>
-            <input type="text" placeholder="Сайт"/>
+            <input name="site" type="text" placeholder="Сайт" value="<?=$club->site;?> " validator="empty"/>
         </div>
         <div class="user-input">
             <label class="caption">Телефон</label>
-            <input type="text" placeholder="Телефон"/>
+            <input name="phone" type="text" placeholder="Телефон" value="<?=$club->phone;?>" validator="empty"/>
         </div>
         <div class="user-input">
             <label class="caption">Город</label>
-            <select id="city-combobox">
+            <select name="cityid" id="city-combobox">
                 <option value="">Город...</option>
                 <? foreach($cities as $city) : ?>
-                    <option value="<?=$city->id?>"><?=$city->name;?></option>
+                    <option value="<?=$city->id?>" <?if($club->cityid == $city->id) echo "selected";?> ><?=$city->name;?></option>
                 <? endforeach;?>
             </select>
         </div>
         <div class="user-input">
             <label class="caption">Район</label>
-            <select id="district-combobox">
+            <select name="districtId" id="district-combobox">
                 <option value="">Район...</option>
                 <? foreach($districts as $distr) : ?>
-                    <option value="<?=$distr->id?>"><?=$distr->name;?></option>
+                    <option value="<?=$distr->id?>" <?if($club->districtId == $distr->id) echo "selected";?>><?=$distr->name;?></option>
                 <? endforeach;?>
             </select>
         </div>
         <div class="user-input">
             <label class="caption">Адрес</label>
-            <input type="text" placeholder="Адрес"/>
+            <input name="address" type="text" placeholder="Адрес" value="<?=$club->address;?>" validator="empty"/>
         </div>
-        <input class="save" id="common-save" type="button" value="Сохранить"/>
-        <label class="result">Изменения отправлены на проверку.</label>
-
+        <input name="clubid" value="<?=$club->id;?>" type="hidden" />
+        <input id="common-save" class="save" type="button" value="Сохранить" />
     </div>
 </div>
 
 <div class="info-block">
     <div class="title">Краткое описание</div>
-    <div class="box"></div>
+    <div class="box">
+        <div class="user-input">
+            <label class="caption">Логотип</label>
+            <div class="lclear"></div>
+            <div class="club-logo-input">
+                <img class="club-logo" src="<?=$club->head_picture?>"></img>
+                <input name="head_picture" type="file" placeholder="Логотип" />
+            </div>
+        </div>
+        <div class="user-input">
+            <label class="caption">Краткое описание</label>
+            <div class="lclear"></div>
+            <textarea class="texteditor" name="description" placeholder="Описание..." /></textarea>
+        </div>
+    </div>
 </div>
 
 <div class="lclear"></div>
