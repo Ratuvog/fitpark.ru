f27c8d196a4fc2dcfb7fd68ce28c5cd2322a66f9
Ratuvog <valkiriya.nd@gmail.com>
2013-12-16 17:30:49 +0400

    Исправлены переходы по табам на странице редактирования клуба

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,3 +1,4 @@
+<!--
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
 <h4>A PHP Error was encountered</h4>
@@ -8,4 +9,5 @@
 <p>Line Number: <?php echo $line; ?></p>
 
 </div>
+-->
  
--- application/views/blocks/content-manager-menu.php
+++ application/views/blocks/content-manager-menu.php
@@ -1,8 +1,8 @@
 <div id="make-better" style="display: block;">
     <div id="help-links" class="inline">
         <p>
-            <a href="<?=site_url(array('Manager/getClub/87/base'))?>">Базовая информация</a>
-            <a href="<?=site_url(array('Manager/getClub/87/photo'));?>">Изображения клуба</a>
+            <a href="<?=site_url(array("Manager/getClub/$club->id/base"))?>">Базовая информация</a>
+            <a href="<?=site_url(array("Manager/getClub/$club->id/photo"));?>">Изображения клуба</a>
         </p>
     </div>
 </div><!--#make-better[END]-->
