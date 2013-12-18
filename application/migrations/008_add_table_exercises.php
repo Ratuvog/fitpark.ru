92bc1e9751339258dbfb4f12879ab64a2e72c12e
Ratuvog <valkiriya.nd@gmail.com>
2013-12-18 15:32:22 +0400

    Добавлена миграция 008. Добавляет таблицу упражнений. Дамп данных из текущей базы добавляется из файла exercise_data_migration_008.query

---
+++ application/migrations/008_add_table_exercises.php
@@ -0,0 +1,41 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_table_exercises extends CI_Migration
+{
+    public function up()
+    {
+        //Структура таблицы `exercises`
+        $this->db->query("
+            CREATE TABLE IF NOT EXISTS `exercises` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `name` varchar(1024) CHARACTER SET utf8 NOT NULL,
+              `image` varchar(255) NOT NULL,
+              `typeId` int(11) NOT NULL,
+              `description` text CHARACTER SET utf8 NOT NULL,
+              `video` varchar(255) CHARACTER SET utf8 NOT NULL,
+              `technique` text CHARACTER SET utf8 NOT NULL,
+              `nuances` text CHARACTER SET utf8 NOT NULL,
+              `image1` varchar(512) NOT NULL,
+              `image2` varchar(512) NOT NULL,
+              `image3` varchar(512) NOT NULL,
+              `image4` varchar(512) NOT NULL,
+              `image5` varchar(512) NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=104 ;
+        ");
+
+        $this->load->helper('file');
+        // Добавление данных в таблицу 'district'
+        if (file_exists("application/migrations/exercise_data_migration_008.query"))
+            $this->db->query(read_file("application/migrations/exercise_data_migration_008.query"));
+    }
+
+    public function down()
+    {
+        // Для быстрого удаления
+        // Сначала очищаем таблицу
+        $this->db->query("TRUNCATE TABLE `district`");
+        // Потом удаляем
+        $this->db->query("DROP TABLE IF EXISTS `district`");
+    }
+}
