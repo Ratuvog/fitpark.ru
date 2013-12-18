43d0ddfa461d489e96014a06c43276f5e50f7c32
Ratuvog <valkiriya.nd@gmail.com>
2013-12-18 17:26:58 +0400

    Добавлена миграция 025. Добавляет таблицу c порядками вывода клубов.

---
+++ application/migrations/025_add_table_order.php
@@ -0,0 +1,33 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_table_order extends CI_Migration
+{
+    public function up()
+    {
+        //Структура таблицы `orders`
+        $this->db->query("
+            CREATE TABLE IF NOT EXISTS `orders` (
+              `id` int(11) NOT NULL DEFAULT '1',
+              `name` varchar(255) NOT NULL,
+              `active` tinyint(1) NOT NULL,
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `id` (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+        ");
+
+        //Данные таблицы `orders`
+        $this->db->query("
+            INSERT INTO `orders` (`id`, `name`, `active`) VALUES
+            (1, 'VNIMANIE! ETA ZAPIS DOLJNA BIT'' EDINSTVENNOI!!!!!!', 1);
+        ");
+    }
+
+    public function down()
+    {
+        // Для быстрого удаления
+        // Сначала очищаем таблицу
+        $this->db->query("TRUNCATE TABLE `orders`");
+        // Потом удаляем
+        $this->db->query("DROP TABLE IF EXISTS `orders`");
+    }
+}
