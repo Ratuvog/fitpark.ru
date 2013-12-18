dfdeff681946ea7c83679d074c854724c23e9fc6
Ratuvog <valkiriya.nd@gmail.com>
2013-12-18 17:37:42 +0400

    Добавлена миграция 028. Добавляет таблицу paidShows

---
+++ application/migrations/028_add_table_paid_shows.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_table_paid_shows extends CI_Migration
+{
+    public function up()
+    {
+        //Структура таблицы `paidShows`
+        $this->db->query("
+            CREATE TABLE IF NOT EXISTS `paidShows` (
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `clubId` int(11) NOT NULL,
+              `paidBlockId` int(11) NOT NULL,
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `clubid_paidBlockId_unique` (`clubId`,`paidBlockId`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+        ");
+
+        //Данные таблицы `paidShows`
+        $this->db->query("
+            INSERT INTO `paidShows` (`id`, `clubId`, `paidBlockId`) VALUES
+            (8, 487, 1),
+            (10, 487, 2);
+        ");
+    }
+
+    public function down()
+    {
+        // Для быстрого удаления
+        // Сначала очищаем таблицу
+        $this->db->query("TRUNCATE TABLE `paidShows`");
+        // Потом удаляем
+        $this->db->query("DROP TABLE IF EXISTS `paidShows`");
+    }
+}
