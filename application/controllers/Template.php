6b4b0b8ac0868bfed9abec5f37eaeda5c8f8f382
dima11221122 <dimansny@bk.ru>
2014-01-12 18:53:26 +0400

    Удадлил бесполезный конструктор

--- application/controllers/Template.php
+++ application/controllers/Template.php
@@ -4,15 +4,6 @@ abstract class Template extends CI_Controller {
            
     public $template = 'templates/base';
     public $output;
-    
-    function __construct()
-    {
-        parent::__construct();
-         // Запуск миграции БД. Текущая версия устанавливается в config/migration.php.
-//        if (!$this->migration->current()){
-//            show_error($this->migration->error_string());
-//        }
-    }
 
     function renderScene()
     {
