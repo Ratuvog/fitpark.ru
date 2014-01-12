2cb7f255c43e7d4dac4976acda87a537ae4a9c8c
dima11221122 <dimansny@bk.ru>
2014-01-12 19:00:08 +0400

    Поправил путь до скрипта автозагрузки composer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once './vendor/autoload.php';
+include_once __DIR__.'/vendor/autoload.php';
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
