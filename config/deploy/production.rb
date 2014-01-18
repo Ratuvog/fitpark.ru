ecbf8cb830614371eeff46d6afd88319e5707ace
dima11221122 <dimansny@bk.ru>
2014-01-18 23:21:55 +0400

    Merge with master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,11 +2,12 @@ set :stage, :production
 server '176.57.210.39', roles: :all, port: 22 
 set :branch, 'production'
 set :deploy_to, '/home/w/whidohost/fitpark.rf'
-namespace my do
-   task custom_task do
+namespace :my do
+   task :custom_task do
        on roles :all do
-        execute :ln, '-s',"#{fetch(:path_to_share)}/yandex_*", "#{fetch(:deploy_to)}/public_html"
-        upload '/home/dmitry/fitpark.ru/fitpark.ru/config/deploy/production/.htaccess', "#{fetch(:deploy_to)}/public_html"
+        execute :ln, '-s', "#{fetch(:path_to_share)}/yandex_*", "#{fetch(:deploy_to)}/public_html"
+        execute :ln, '-s', "#{fetch(:path_to_share)}/.htaccess","#{fetch(:deploy_to)}/public_html"
+        #upload '/home/dmitry/fitpark.ru/fitpark.ru/config/deploy/production/.htaccess', "#{fetch(:deploy_to)}/public_html"
        end
    end
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :stage, :staging
 set :branch, 'master'
 server '176.57.210.39', roles: :all, port: 22 
 set :deploy_to, '/home/w/whidohost/demo.fitpark.rf'
-namespace my do
-   task custom_task do
+namespace :my do
+   task :custom_task do
    end
 end
