5f8a0b304c009e29be730b6e3981ba70b00ac6ed
dima11221122 <dimansny@bk.ru>
2014-01-31 01:39:26 +0400

    asdfads

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -17,36 +17,21 @@ class About  extends Base
         parent::__construct();
         $this->load->model("sales_model");
         $this->load->model("club_model");
-        $this->title = sprintf("О нас. ФитПарк. %s Тренажерные залы, фитнес центры,
-                                отзывы, стоимость, рейтинги, акции, скидки.",
-            lang('title'));
-
-        $this->description = sprintf("%s. Отзывы, рейтинг, фотографии, цены, описание.",
-            lang("common_desc"));
-
-        $this->keywords = sprintf("%s. Бассейн, тренажерный зал, аэробика,
-                                   танцы, йога, пилатес, тренажеры.",
-            lang("common_keys"));
+        $this->title       = sprintf($this->title       , lang('title'));
+        $this->description = sprintf($this->description , lang("common_desc"));
+        $this->keywords    = sprintf($this->keywords    , lang("common_keys"));
     }
 
+    /**
+     *@MetaInfo\DescriptionReader\Annotation\MetaInfo(value="index")
+     */
     function index()
     {
-        $this->breadcrumbs []= (object)array(
-            'name' => "Главная",
-            'url' => base_url()
-        );
-
-        $this->breadcrumbs []= (object)array(
-            'name' => "О нас",
-            'url'  => site_url('about')
-        );
-
         $this->content->view = $this->view;
         $this->content->data->content_title->title = 'О нас';
         $this->content->data->breadcrumbs->stack = $this->breadcrumbs;
         $this->renderScene();
     }
-
 }
 
 ?>
